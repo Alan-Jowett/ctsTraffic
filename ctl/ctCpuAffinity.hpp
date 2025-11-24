7ee493052eff670492c285d1fc67a84bc2188199
Alan Jowett <alan.jowett@microsoft.com>
2025-11-24 08:31:47 -0800

    Licensing: add Apache-2.0 header to new ctCpuAffinity sources and tests

--- ctl/ctCpuAffinity.hpp
+++ ctl/ctCpuAffinity.hpp
@@ -1,8 +1,21 @@
+/*
+
+Copyright (c) Microsoft Corporation
+All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the ""License""); you may not use this file except in compliance with the License. You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+
+THIS CODE IS PROVIDED ON AN  *AS IS* BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION ANY IMPLIED WARRANTIES OR CONDITIONS OF TITLE, FITNESS FOR A PARTICULAR PURPOSE, MERCHANTABLITY OR NON-INFRINGEMENT.
+
+See the Apache Version 2.0 License for specific language governing permissions and limitations under the License.
+
+*/
+
 /* ctCpuAffinity - helper to detect CPU affinity facilities and map shard -> affinity
-   This header provides a small, testable interface for the sharded receive
-   implementation to detect kernel capabilities (SIO_CPU_AFFINITY) and compute
-   per-shard affinity mappings. Implementations can be platform-optimized in
-   a corresponding .cpp file.
+    This header provides a small, testable interface for the sharded receive
+    implementation to detect kernel capabilities (SIO_CPU_AFFINITY) and compute
+    per-shard affinity mappings. Implementations can be platform-optimized in
+    a corresponding .cpp file.
 */
 
 #pragma once
